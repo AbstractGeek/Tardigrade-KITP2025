27fee26587e71ddd1725f2d8722d8e1324bf08ed
Dinesh Natesan <AbstractGeek@users.noreply.github.com>
2025-07-10 20:51:33 -0700

    working led toggle

--- led_basic/led_basic.ino
+++ led_basic/led_basic.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Bounce2.h>
 
 #define RED_PIN 11    // Pin for red channel of RGB LED
 #define GREEN_PIN 10  // Pin for green channel of RGB LED
@@ -6,18 +7,15 @@
 #define BUTTON_PIN 2  // Pin for button input
 
 // Define brightness levels for each color channel (0-255)
-int red_brightness = 255;
+int red_brightness = 10;
 int green_brightness = 0;
 int blue_brightness = 0;
 
-// toggle related
-int ledState = HIGH;        // the current state of the output pin
-int buttonState = LOW;            // the current reading from the input pin
-int lastButtonState = LOW;  // the previous reading from the input pin
+// Bounce instance for button debouncing
+Bounce button = Bounce();
 
-// debounce parameters
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 100;    // the debounce time; increase if the output flickers
+// Variable to store the LED state
+bool ledState = HIGH;
 
 void setup() {
   // Set RGB pins as outputs
@@ -25,18 +23,47 @@ void setup() {
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
 
-  // Set the RGB LED to initial color (all off)
-  analogWrite(RED_PIN, red_brightness);     // Set red brightness
-  analogWrite(GREEN_PIN, green_brightness); // Set green brightness
-  analogWrite(BLUE_PIN, blue_brightness);   // Set blue brightness
+  // Set the RGB LED to initial color
+  analogWrite(RED_PIN, red_brightness);
+  analogWrite(GREEN_PIN, green_brightness);
+  analogWrite(BLUE_PIN, blue_brightness);
+
+  // Attach the Bounce instance to the button pin (external pull-down or pull-up)
+  button.attach(BUTTON_PIN, INPUT_PULLUP);
+  button.interval(5); // debounce interval in ms
 
   // Begin serial communication for debugging and control
   Serial.begin(9600);
-  Serial.println("Initialized RGB LEDs.");
+  Serial.print("Initialized RGB LEDs to: ");
+  Serial.print(red_brightness); Serial.print(", ");
+  Serial.print(green_brightness); Serial.print(", ");
+  Serial.println(blue_brightness);
 }
 
 void loop() {
-  
+  // Update the Bounce instance (MUST be called every loop)
+  button.update();
+
+  // If the button state changed (from HIGH to LOW or LOW to HIGH)
+  if (button.changed()) {
+    int debouncedInput = button.read();
+    // If the changed value is LOW (button pressed)
+    if (debouncedInput == LOW) {
+      ledState = !ledState; // Toggle LED state
+      if (ledState == HIGH) {
+        analogWrite(RED_PIN, red_brightness);
+        analogWrite(GREEN_PIN, green_brightness);
+        analogWrite(BLUE_PIN, blue_brightness);
+        Serial.println("Button pressed: LEDs ON");
+      } else {
+        analogWrite(RED_PIN, 0);
+        analogWrite(GREEN_PIN, 0);
+        analogWrite(BLUE_PIN, 0);
+        Serial.println("Button pressed: LEDs OFF");
+      }
+    }
+  }
+
   // Check for serial input to update LED brightness
   // Input format: R,G,B (e.g., 128,64,255)
   if (Serial.available() > 0) {
@@ -59,9 +86,8 @@ void loop() {
         analogWrite(GREEN_PIN, green_brightness);
         analogWrite(BLUE_PIN, blue_brightness);
       }
-
       // Print confirmation to serial monitor
-      Serial.print("Updated RGB to: ");
+      Serial.print("Updated RGB LEDs to: ");
       Serial.print(red_brightness); Serial.print(", ");
       Serial.print(green_brightness); Serial.print(", ");
       Serial.println(blue_brightness);
@@ -70,36 +96,4 @@ void loop() {
       Serial.println("Invalid input. Use format: R,G,B");
     }
   }
-
-  // Read the state of the button into a local variable:
-  int reading = digitalRead(BUTTON_PIN);
-
-  // If the button state has changed (due to noise or pressing):
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis();
-  }
-
-  // Only act if the button state has been stable for debounceDelay
-// Only act if the button state has been stable for debounceDelay but less than 2*debounceDelay
-if ((millis() - lastDebounceTime) > debounceDelay && (millis() - lastDebounceTime) < 2 * debounceDelay) {
-    // If the button state has changed:
-    if (reading != buttonState) {
-      buttonState = reading;
-      // Only toggle the LED if the new button state is HIGH (button pressed)
-      if (buttonState == HIGH) {
-        ledState = !ledState;
-        if (ledState == HIGH) {
-          analogWrite(RED_PIN, red_brightness);
-          analogWrite(GREEN_PIN, green_brightness);
-          analogWrite(BLUE_PIN, blue_brightness);
-        } else {
-          analogWrite(RED_PIN, 0);
-          analogWrite(GREEN_PIN, 0);
-          analogWrite(BLUE_PIN, 0);
-        }
-      }
-    }
-  }
-  // Save the reading for next loop iteration
-  lastButtonState = reading;
 }
